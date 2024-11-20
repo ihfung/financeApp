b0840376b3e79b7708c0c915f48c9bafeacde4fe
ihleung <ihfungleung@gmail.com>
2024-11-20 12:36:17 -0500

    bottom navigation

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,7 +5,7 @@ import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 export default function RootLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Move Money',  tabBarIcon: ({size, color})=> <MaterialIcons name="attach-money" size={size} color="color" />}} />
+      <Tabs.Screen name="index" options={{ title: 'MoveMoney',  tabBarIcon: ({size, color})=> <MaterialIcons name="attach-money" size={size} color="color" />}} />
       <Tabs.Screen name="accounts" options={{title: 'Accounts', tabBarIcon: ({size, color})=> <MaterialCommunityIcons name="account" size={size} color="color" />}} />
     </Tabs>
   );
